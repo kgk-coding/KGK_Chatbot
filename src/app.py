ab294be3f871e71ea180ba8b699d6244530c974d
Köksal Gürkan <koksalgurkankocluk@gmail.com>
2025-10-22 21:54:18 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from rag_chain import retrieve_answer
 
-st.set_page_config(page_title="Koçluk Chatbot", layout="centered")
-st.title("Koçluk Chatbot")
+st.set_page_config(page_title="KGK Koçluk Chatbot", layout="centered")
+st.title("KGK Kocluk Chatbot")
+st.write("Bu chatbot, koçlukla ilgili sorularınızı https://www.koksalgurkan.com.tr/ sitesi ve kişisel deneyimlerimi referans alarak hazırlanmış özel içerik dokümandaki bilgilere göre yanıtlar.")
+
 
 # Enter ile cevaplama
 user_input = st.text_input("Sorunuzu yazın ve Enter'a basın:")
